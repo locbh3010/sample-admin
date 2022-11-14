49c50509e2cb274b8074d7e852bd5c1b860f4a18
locbh3010 <locbh3010@gmail.com>
2022-11-14 14:56:13 +0700

    hoan thanh input control

--- src/components/ui/input/Input.jsx
+++ src/components/ui/input/Input.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { useController } from "react-hook-form";
 
 export default function Input({
   type = "text",
   display,
   placeholder,
   name,
+  control,
+  defaultValue = "",
   props,
 }) {
+  const { field } = useController({
+    name,
+    control,
+    defaultValue,
+  });
   return (
     <div className="flex flex-col gap-2">
       <label htmlFor={name} className="font-semibold text-xl capitalize">
@@ -18,6 +26,7 @@ export default function Input({
         id={name}
         placeholder={placeholder}
         className="w-full outline-none bg-transparent px-3 py-4 bg-gray-300 border border-gray-400 rounded duration-300 focus:bg-transparent focus:shadow"
+        {...field}
         {...props}
       />
     </div>
